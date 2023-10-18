3dc3d54c773e26a711af4083439bfc9143bf9866
priyanka921805 <priyanka921805verma@gmail.com>
2023-10-18 11:11:05 +0530

    Modification in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,14 +7,14 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">{props.title}</a>
+    <a className="navbar-brand" href="/">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         {/* <li className="nav-item">
           <a className="nav-link" href="/aboutText">{props.aboutText}</a>
@@ -29,7 +29,7 @@ export default function Navbar(props) {
         <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Red Color </label>
         </div>
         <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
-        <input className="form-check-input btn btn-warning  mx-1" onClick={props.toggleMode}type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+        <input className="form-check-input btn btn-warning mx-1" onClick={props.toggleMode}type="checkbox" role="switch" id="flexSwitchCheckDefault" />
         <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Yellow Color </label>
         </div>
     <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
